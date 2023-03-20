10597fbfcb50a5ed257718c91be308f002bf960f
unknown <unluckypleasure@yandex.ru>
2023-03-21 00:23:10 +0300

    starting navbar

--- shmelcom/src/features/header/view/index.tsx
+++ shmelcom/src/features/header/view/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable import/prefer-default-export */
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Flex } from '../../../shared/ui';
 import {
   SearchIcon,
@@ -9,21 +11,62 @@ import {
   CartIcon,
 } from '../../../shared/assets';
 
+const navigation = [
+  { name: 'All', href: '#', current: true },
+  { name: 'New Arrivals', href: '#', current: false },
+  { name: 'T-Shirts', href: '#', current: false },
+  { name: 'Hoodies', href: '#', current: false },
+  { name: 'Bottoms', href: '#', current: false },
+  { name: 'Collectibles', href: '#', current: false },
+  { name: 'Extras', href: '#', current: false },
+  { name: 'ShmelFarms', href: '#', current: false },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
 export function Header() {
   return (
-    <Flex className="pt-[132px] pl-[100px] gap-[250px]">
-      <Flex className="gap-[40px]">
-        <SearchIcon />
-        <MoonIcon />
+    <Flex className="flex-col">
+      <Flex className="pt-[132px] pl-[100px] gap-[250px]">
+        <Flex className="gap-[40px]">
+          <SearchIcon />
+          <MoonIcon />
+        </Flex>
+        <Flex>
+          <ShmelIcon />
+        </Flex>
+        <Flex className="gap-[40px]">
+          <ProfileIcon />
+          <CartIcon />
+        </Flex>
+        <Flex />
       </Flex>
-      <Flex className="relative ">
-        <ShmelIcon />
-      </Flex>
-      <Flex className="gap-[40px]">
-        <ProfileIcon />
-        <CartIcon />
-      </Flex>
-      <Flex />
+      <Disclosure as="nav" className="pl-[100px] pt-[50px]">
+        {() => (
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center" />
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-[100px]">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </Disclosure>
     </Flex>
   );
 }
